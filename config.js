4655498d3ba61065c634a8dd585148c80ffdc2ea
Evvy Xan <144759148+Evvyinnit@users.noreply.github.com>
2025-01-31 20:12:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-MopRQZSa#xxOP1LX15AwQZJiycxaJfWF-GSLKYCs9LfI2FrWUQ-Aftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://evvyxan:Q3MzeKWy9ZnRu6QV@cluster0.7kwla.mongodb.net/",
 
 
 MAX_SIZE: 500,
